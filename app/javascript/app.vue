c186f764c4b535fbcce8fda1857358d24b8fc0c6
Micah Rothenbuhler <micahrothebuhler@gmail.com>
2021-02-16 18:38:58 -0800

    Finish up auth stuff

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -9,20 +9,28 @@
 export default {
   data: function() {
     return {
-      // message: "Hello Vue!"
     };
   },
   beforeCreate() {
     const existingSession = this.$cookie.get('session')
 
-    if (existingSession && existingSession.length) { // A string at this point
+    if (existingSession && existingSession.length && JSON.parse(existingSession) != null) {
       const session = JSON.parse(existingSession)
       console.log("current session: " + session);
       this.$store.commit('user', session.user)
       this.$store.commit('auth', session.tokens)
       this.$router.push({ name: 'dashboard' })
     } else {
-      this.$router.push({ name: 'login' })
+      this.$router.push({ name: 'login' }).catch(err => {
+        // Ignore the vuex err regarding  navigating to the page they are already on.
+        if (
+          err.name !== 'NavigationDuplicated' &&
+          !err.message.includes('Avoided redundant navigation to current location')
+        ) {
+          // But print any other errors to the console
+          console.error(err);
+        }
+      });
     }
   },
 };
--- app/javascript/packs/components/sessions/login.vue
+++ app/javascript/packs/components/sessions/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="max-w-sm m-auto my-8">
     <div class="border p-10 border-grey-light shadow rounded">
-      <h3 class="text-2xl mb-6 text-grey-darkest">Sign In NOW</h3>
+      <h3 class="text-2xl mb-6 text-grey-darkest">Sign In</h3>
       <form @submit.prevent="signin">
         <div class="text-red" v-if="error">{{ error }}</div>
 
