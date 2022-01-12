5c983594b5b777b4544476b6e9fbbda139d91f8d
Micah Rothenbuhler <micahrothebuhler@gmail.com>
2022-01-12 14:31:46 -0800

    Fix ESLint bugs (basically just remove for now)

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,12 +5,11 @@ const vue = require('./loaders/vue')
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 
-const { environment } = require('@rails/webpacker')
-const vue =  require('./loaders/vue')
-const eslint =  require('./loaders/eslint')
-
-environment.loaders.append('vue', vue)
-
-environment.loaders.append('eslint', eslint)
+// uncomment for eslint:
+// const { environment } = require('@rails/webpacker')
+// const vue =  require('./loaders/vue')
+// const eslint =  require('./loaders/eslint')
+// environment.loaders.append('vue', vue)
+// environment.loaders.append('eslint', eslint)
 
 module.exports = environment
